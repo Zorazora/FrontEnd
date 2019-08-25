a637c311a2e3545a5cb56ce7e78ec8fc9049b6de
zorazora <1151577229@qq.com>
2019-08-25 17:35:33 +0800

    add: 登录前后端连接

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   registerForm: FormGroup;
   private login = true;
+  private info = '';
 
   constructor(private fb: FormBuilder, public router: Router, private http: HttpClient) { }
 
@@ -28,22 +29,51 @@ export class LoginComponent implements OnInit {
   }
 
   submitForm(): void {
-    // for (const control of this.validateForm.controls) {
-    //   control.markAsDirty();
-    //   control.updateValueAndValidity();
-    // }
-    console.log('submit');
-    sessionStorage.setItem('userID', 'admin');
-    this.router.navigate(['']);
-  }
-
-  register(): void {
-    let registInfo;
-    registInfo = {username: this.registerForm.getRawValue().userName, password: this.registerForm.getRawValue().password};
-    this.http.post('/api/user/register', registInfo).subscribe(data => {
-      console.log(data);
+    let logInfo;
+    logInfo = {username: this.loginForm.getRawValue().userName, password: this.loginForm.getRawValue().password};
+    this.http.post('/api/user/login', logInfo).subscribe(data => {
+      if (data.message === 'SUCCESS') {
+        sessionStorage.setItem('username', logInfo.username);
+        this.router.navigate(['']);
+      } else {
+        this.info = 'wrong';
+        setTimeout(() => {
+          this.info = '';
+        }, 5000);
+      }
     }, error => {
       console.log(error);
     });
+  }
+
+  register(): void {
+    if (this.registerForm.getRawValue().password !== this.registerForm.getRawValue().passwordAgain) {
+      this.info = 'error';
+      setTimeout(() => {
+        this.info = '';
+      }, 5000);
+    } else {
+      let registInfo;
+      registInfo = {username: this.registerForm.getRawValue().userName, password: this.registerForm.getRawValue().password};
+      this.http.post('/api/user/register', registInfo).subscribe(data => {
+        switch (data.message) {
+          case 'SUCCESS':
+            this.info = 'success';
+            this.login = true;
+            break;
+          case 'FAIL':
+            this.info = 'wrong';
+            break;
+          case 'FAIL_EXISTED':
+            this.info = 'existed';
+            break;
+        }
+        setTimeout(() => {
+          this.info = '';
+        }, 5000);
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-room-list',
@@ -12,18 +13,30 @@ export class RoomListComponent implements OnInit {
   public roomType;
   public roomList = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
     if (sessionStorage.getItem('username') === null) {
       this.router.navigate(['login']);
     }
+    this.loadData();
   }
 
   createRoom(): void {
     let roomInfo;
     roomInfo = {};
     roomInfo.type = this.roomType;
+    roomInfo.username = sessionStorage.getItem('username');
+  }
+
+  loadData(): void {
+    this.http.get('/api/room/roomList', {username: sessionStorage.getItem('username')}).subscribe(data => {
+      this.roomList = data.roomList;
+      console.log(this.roomList);
+      console.log(data.roomList);
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
